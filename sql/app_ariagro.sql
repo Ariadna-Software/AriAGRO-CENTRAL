7510fe906227e365d7ed38d456452da9ae8e453c
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2016-05-18 12:13:21 +0200

    Cambio de mensajes

---
+++ sql/app_ariagro.sql
@@ -0,0 +1,35 @@
+/*
+SQLyog Community v12.2.1 (64 bit)
+MySQL - 5.6.16 : Database - app_ariagro
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+USE `app_ariagro`;
+
+/*Table structure for table `parametros` */
+
+DROP TABLE IF EXISTS `parametros`;
+
+CREATE TABLE `parametros` (
+  `parametroId` int(11) NOT NULL,
+  `url` varchar(255) DEFAULT NULL,
+  `nombre` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`parametroId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `parametros` */
+
+insert  into `parametros`(`parametroId`,`url`,`nombre`) values (0,'http://ariadna.myariadna.com:8021','ALZICOOP');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
